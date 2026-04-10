3050505f071471c2b7f0849a276fd4c276a6d48d
kenali <kanan1294@gmail.com>
2026-04-10 17:45:54 +0300

    organise sructure of folder & fixed tailwind configure issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Serif, Mona_Sans } from "next/font/google";
 
-import Navbar from "@/components/Navbar";
+import Navbar from "../components/Navbar";
 import "./globals.css";
 
 const ibmPlexSerif = IBM_Plex_Serif({
